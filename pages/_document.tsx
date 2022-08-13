9c8b1d0cff08fd44ec4c30d6e7824122dfb72c36
dieptv <diep.tv1999@gmail.com>
2022-08-13 22:32:46 +0700

    .

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,8 @@
-import Document, { DocumentContext, DocumentInitialProps } from 'next/document';
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+} from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class StyledDocument extends Document {
@@ -20,6 +24,23 @@ export default class StyledDocument extends Document {
         ...initialProps,
         styles: (
           <>
+            <Head>
+              <script
+                async
+                src="https://www.googletagmanager.com/gtag/js?id=G-Q2MGPQX0JE"
+              />
+
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '[Tracking ID]', { page_path: window.location.pathname });
+            `,
+                }}
+              />
+            </Head>
             {initialProps.styles}
             {sheet.getStyleElement()}
           </>
