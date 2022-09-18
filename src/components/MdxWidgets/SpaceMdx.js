f072f03b09068e4f7c35943909e839457a6bbf89
dieptv <diep.tv1999@gmail.com>
2022-09-18 14:26:20 +0700

    .

--- src/components/MdxWidgets/SpaceMdx.js
+++ src/components/MdxWidgets/SpaceMdx.js
@@ -1,9 +1,7 @@
+export const SpaceMdx = ({ size = 'medium' }) => {
+  return (
+    <div style={{ height: size === 'medium' ? 30 : size === 'large' ? 40 : 20 }}>
 
-
-export const SpaceMdx = ({size = 'medium'}) => {
-    return (
-    <div style={{height: size === 'medium' ? 30 : size === large ? 40 : 20}}>
-
-        </div>
-    )
-}
+    </div>
+  );
+};
