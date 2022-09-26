efc31a1347ae0b1baa212f76548dc148203a0a5f
dieptv1999 <diep.tv199@gmail.com>
2022-09-26 09:57:23 +0700

    fix css

--- src/pages/_app.page.js
+++ src/pages/_app.page.js
@@ -16,6 +16,7 @@ import { msToNum } from 'utils/style';
 import { ScrollRestore } from '../layouts/App/ScrollRestore';
 import Script from 'next/script';
 import { DefaultSeo } from 'next-seo';
+import '../../styles.css';
 import dynamic from 'next/dynamic';
 // const Music = dynamic(() => import('../components/Music').then(mod => mod.Music), {ssr: false});
 
