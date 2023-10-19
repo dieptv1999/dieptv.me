3cd0fb491309397428be44ae88e2fb13d901574e
dieptv1999 <diep.tv1999@gmail.com>
2023-10-19 20:31:04 +0700

    .

--- src/pages/_document.page.js
+++ src/pages/_document.page.js
@@ -2,6 +2,7 @@ import GothamBook from 'assets/fonts/gotham-book.woff2';
 import GothamMedium from 'assets/fonts/gotham-medium.woff2';
 import { fontStyles, tokenStyles } from 'components/ThemeProvider';
 import { Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -22,6 +23,28 @@ export default function Document() {
         <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400&display=swap" rel="stylesheet" />
         <style dangerouslySetInnerHTML={{ __html: fontStyles }} />
         <style dangerouslySetInnerHTML={{ __html: tokenStyles }} />
+        <Script
+          src='https://www.googletagmanager.com/gtag/js?id=GTM-54R4KKL2'
+          strategy='afterInteractive'
+        />
+        <Script id='google-analytics' strategy='afterInteractive'>
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'GTM-54R4KKL2');
+        `}
+        </Script>
+        <Script id='google-tag-manager' strategy='afterInteractive'>
+          {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-54R4KKL2');
+      `}
+        </Script>
       </Head>
       <body data-theme='dark' tabIndex={-1}>
       <script
@@ -34,12 +57,12 @@ export default function Document() {
       />
       <Main />
       <NextScript />
+      <div id='portal-root' />
       <noscript
         dangerouslySetInnerHTML={{
           __html: `<iframe src='https://www.googletagmanager.com/ns.html?id=GTM-54R4KKL2' height='0' width='0' style='display:none;visibility:hidden'></iframe>`,
         }}
       />
-      <div id='portal-root' />
       </body>
     </Html>
   );
