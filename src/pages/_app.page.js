0f46e41a8b0ee8aafccd8596a05dc60e3a2e3843
dieptv1999 <diep.tv1999@gmail.com>
2023-11-29 22:41:39 +0700

    init

--- src/pages/_app.page.js
+++ src/pages/_app.page.js
@@ -63,6 +63,28 @@ const App = ({ Component, pageProps }) => {
   return (
     <AppContext.Provider value={{ ...state, dispatch }}>
       <ThemeProvider themeId={state.theme}>
+        <Script
+          src='https://www.googletagmanager.com/gtag/js?id=GTM-54R4KKL2'
+          strategy='afterInteractive'
+        />
+        <Script id='google-analytics' strategy='afterInteractive'>
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'GTM-54R4KKL2');
+        `}
+        </Script>
+        <Script id='google-tag-manager' strategy='afterInteractive'>
+          {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-54R4KKL2');
+      `}
+        </Script>
         <LazyMotion features={domAnimation}>
           <Fragment>
             <DefaultSeo {...seo} />
